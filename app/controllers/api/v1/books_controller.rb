5437989e1f2472908b16d94caa77865f8db15d98
Júlia Cabral <juliacabral61@gmail.com>
2025-08-04 18:44:13 -0300

    fix: remove unnecessary comment

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::BooksController < ApplicationController
   def index
     @books = Book.all
-    # render json: @books, serializer: BookSerializer, status: :ok
     render json: BookSerializer.new(@books).serializable_hash
   end
 end
