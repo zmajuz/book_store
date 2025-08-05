7b44085a4d1cec4af28ea0e785012930266b10ec
Júlia Cabral <juliacabral61@gmail.com>
2025-08-05 09:19:25 -0300

    fix: remove unnecessary '@'

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    @books = Book.all
-    render json: BookSerializer.new(@books).serializable_hash
+    books = Book.all
+    render json: BookSerializer.new(books).serializable_hash
   end
 end
