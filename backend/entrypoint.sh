e7a43ecf45bca5abc16bd0d20fe0f71694808733
Weslley Campos <weslley.campos@rebase.com.br>
2025-08-13 09:54:08 -0300

    dev: fix bundler permissions by chown in entrypoint and run as root then drop to appuser

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-rm -f tmp/pids/server.pid
+if [ "$(id -u)" = "0" ]; then
+	mkdir -p /usr/local/bundle /app/tmp/pids
+	chown -R appuser:appuser /usr/local/bundle /app
+	su -s /bin/bash -c 'rm -f tmp/pids/server.pid' appuser
+	exec su -s /bin/bash -c "$*" appuser
+fi
 
+rm -f tmp/pids/server.pid
 exec "$@"
