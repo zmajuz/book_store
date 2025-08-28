0e3d2a353f09eb7ab71259612e6f3a8cd52cf039
Júlia Cabral <juliacabral61@gmail.com>
2025-08-28 08:04:36 -0300

    feat: add list of books

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -1,14 +1,20 @@
 export default function BookCard({ book }) {
     return (
-      <div className="border rounded-lg p-4 shadow hover:shadow-lg transition flex flex-col items-center">
+      <div className="bg-white rounded-lg hover:shadow-lg transition flex flex-col items-center">
+      {/* <div className="bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition flex flex-col items-center"> */}
         <img
-          src={book.imageUrl}
+          src="https://img.travessa.com.br/livro/BA/e2/e2d0eb97-d78a-4d9b-8e23-3cef59bef1bf.jpg"
           alt={book.title}
-          className="w-48 h-64 object-cover mb-4"
+          className="w-full h-64 object-contain mb-1"
         />
-        <h3 className="text-lg font-bold text-center">{book.title}</h3>
-        <p className="text-gray-600 text-center mb-2">{book.author}</p>
-        <span className="text-red-600 font-semibold">{book.price}</span>
+        <h3 className="text-base font-semibold text-center text-gray-800 mb-1">{book.title}</h3>
+        <p className="text-sm text-gray-500 text-center mb-2">{book.author}</p>
+        <span className="text-red-600 font-bold">
+        {Number(book.price).toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </span>
       </div>
     );
 }
--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -2,7 +2,7 @@ import BookCard from "../BookCard/BookCard";
 
 export default function BookList({ books }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-30 gap-x-30 justify-items-center">
       {books.map((book) => (
         <BookCard key={book.id} book={book} />
       ))}
